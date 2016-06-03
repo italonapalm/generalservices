d3fb82a5eff038b4ca09ea81eedbf13a3fb28324
Italo Pires <italonapalm@gmail.com>
2016-06-03 17:01:59 -0300

    autentication + models profession e knowledge

---
+++ db/migrate/20160603191237_create_knowledges.rb
@@ -0,0 +1,10 @@
+class CreateKnowledges < ActiveRecord::Migration
+  def change
+    create_table :knowledges do |t|
+      t.string :description      
+      t.references :profession, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
